c62b4d6ec713fc7c4f20a1d468a64cd2bac9efb7
Vinay Nadig <vinay.nadig@icicletech.com>
2016-01-31 00:14:24 +0530

    Moved bookmark saving to a separate function.

--- scripts/bookmarker.js
+++ scripts/bookmarker.js
@@ -1,59 +1,63 @@
+var saveBookmarks = function() {
+  parser = get_parser(document.URL)
+  host_name = parser.hostname
+  chrome.storage.sync.get(host_name, function (bmObjFromStorage) {
+    var bmObj = bmObjFromStorage || {},
+      currScrollPos = document.body.scrollTop,
+      visibleElements = [],
+      anchorElements = [],
+      timeNow = new Date,
+      currURLParser = get_parser(document.URL)
+
+    if (bmObj[host_name] === undefined) {
+      bmObj[host_name] = {
+        saved_instances: []
+      }
+    }
+
+    conditionalIndex = 0
+    $('body')
+      .find('*')
+      .not('a')
+      .each(function () {
+        if (isElementInViewport($(this)) && $(this).is(':visible') && $(this)[0].id != '' && !($(this).closest('div').css('position') == 'fixed')) {
+          conditionalIndex += 1
+          if(conditionalIndex >= 3) {
+            return false
+          }
+          visibleElements.push($(this)[0])
+        }
+      })
+
+    $.each(visibleElements, function (index, elem) {
+      if (elem.href === "") {
+        return true
+      }
+      anchorElements.push({
+        id: elem.id,
+        text: elem.text,
+        uuid: generate_uuid()
+      })
+    })
+
+    bmObj[host_name]['saved_instances'].push({
+      url: currURLParser.protocol + '//' + currURLParser.host + currURLParser.pathname + currURLParser.search,
+      host_url: currURLParser.protocol + '//' + currURLParser.host,
+      time: timeNow.toJSON(),
+      links: anchorElements,
+      pos: currScrollPos,
+      uuid: generate_uuid()
+    })
+
+    chrome.storage.sync.set(bmObj)
+  })
+}
+
 var handleKeyDown = function (e) {
   var ctrlKey = e.ctrlKey || e.metaKey;
   var shiftKey = e.shiftKey;
   if (e.keyCode === K_KEY_CODE && ctrlKey && shiftKey) {
-    parser = get_parser(document.URL)
-    host_name = parser.hostname
-    chrome.storage.sync.get(host_name, function (bmObjFromStorage) {
-      var bmObj = bmObjFromStorage || {},
-        currScrollPos = document.body.scrollTop,
-        visibleElements = [],
-        anchorElements = [],
-        timeNow = new Date,
-        currURLParser = get_parser(document.URL)
-
-      if (bmObj[host_name] === undefined) {
-        bmObj[host_name] = {
-          saved_instances: []
-        }
-      }
-
-      conditionalIndex = 0
-      $('body')
-        .find('*')
-        .not('a')
-        .each(function () {
-          if (isElementInViewport($(this)) && $(this).is(':visible') && $(this)[0].id != '' && !($(this).closest('div').css('position') == 'fixed')) {
-            conditionalIndex += 1
-            if(conditionalIndex >= 3) {
-              return false
-            }
-            visibleElements.push($(this)[0])
-          }
-        })
-
-      $.each(visibleElements, function (index, elem) {
-        if (elem.href === "") {
-          return true
-        }
-        anchorElements.push({
-          id: elem.id,
-          text: elem.text,
-          uuid: generate_uuid()
-        })
-      })
-
-      bmObj[host_name]['saved_instances'].push({
-        url: currURLParser.protocol + '//' + currURLParser.host + currURLParser.pathname + currURLParser.search,
-        host_url: currURLParser.protocol + '//' + currURLParser.host,
-        time: timeNow.toJSON(),
-        links: anchorElements,
-        pos: currScrollPos,
-        uuid: generate_uuid()
-      })
-
-      chrome.storage.sync.set(bmObj)
-    })
+    saveBookmarks()
   } else if (e.keyCode === L_KEY_CODE && ctrlKey && shiftKey) {
     chrome.runtime.sendMessage("options-tab")
   }
